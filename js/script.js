1c59b83a52297144763fa0d9af6a15262c70b632
Andrii Bereza <amba.vsegda@gmail.com>
2023-01-11 16:41:35 +0200

    trying myself

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+do {
+    console.log(num);
+    num++;
+}
+while(num < 52);
+// for (let i = 0; i < 10; i+2) {
+//     console.log(i);
+// }
